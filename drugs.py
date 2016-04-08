75576fd7ba90490e5120b3124c907c7de035e79f
Becca Ricks <baricks@gmail.com>
2016-04-07 22:37:51 -0400

    updated .py program

--- drugs.py
+++ drugs.py
@@ -3,131 +3,108 @@ import json
 import sys
 import random
 
-#user inputs (1) their name; and (2) their condition/disease.
+#make a function to strip each text
+#make a function to get a random word
+def randomize(lists):
+    random_line = random.choice(lists)
+    return random_line
+
+#make a function to build the drug name
+def druggie(name_starts, name_parts, name_endings):
+    name_1 = randomize(name_starts)
+    name_2 = randomize(name_parts).lower()
+    name_3 = randomize(name_parts).lower()
+    name_4 = randomize(name_endings)
+    return name_1 + name_2 + name_3 + name_4
+
 name = sys.argv[1]
 word = sys.argv[2]
 word_2 = sys.argv[3]
-complete_word = word+" "+word_2
+complete_disorder = word+" "+word_2
 
-#build the drug name
+#build the url
 url = "https://api.fda.gov/drug/event.json?api_key=eijdKy1R2alvbAxzgTnSQP9UOtPXMRJfTLZ0zIzS&search=patient.drug.drugindication:"+word+"&limit=25"
 
 response = urllib.urlopen(url).read()
 data = json.loads(response)
 
-i = random.randint(0,25)
+#build the drug name
+i = random.randint(0, 25)
 for drug in data['results'][i]['patient']['drug'][0]['openfda']['generic_name']:
-	name_1 = drug[0:3]
-	name_2 = drug[3:len(drug)]
-	
+    name_1 = drug[0:3]
+    name_2 = drug[3:len(drug)]
+
 name_parts = [name_1, name_2]
 
 name_endings = list()
 for line in open('name_endings.txt'):
-	name_endings.append(line.strip())
+    name_endings.append(line.strip())
 
 name_starts = list()
 for line in open('name_starts.txt'):
-	name_starts.append(line.strip())
+    name_starts.append(line.strip())
 
-drug_name = random.choice(name_starts) + random.choice(name_parts).lower() + random.choice(name_parts).lower() + random.choice(name_endings)
+drug_name = druggie(name_starts, name_parts, name_endings)
+#drug_name = randomize(name_starts) + randomize(name_parts).lower() + randomize(name_parts).lower() + randomize(name_endings)
+
+#########################################################################
 
 #drug-related adjectives
 adjectives = list()
 for line in open('drug_adj.txt'):
-	adjectives.append(line.strip())
-random_drug_adj = random.choice(adjectives)
+    adjectives.append(line.strip())
 
 #descriptions
 description_sentence = list()
 for line in open('description.txt'):
-	description_sentence.append(line.strip())
-random_description_sentence = random.choice(description_sentence)
+    description_sentence.append(line.strip())
 
 #body parts
 body_parts = list()
 for line in open('body_parts.txt'):
-	body_parts.append(line.strip())
-random_body_part = random.choice(body_parts)
-random_body_part_b = random.choice(body_parts)
-random_body_part_c = random.choice(body_parts)
-random_body_part_d = random.choice(body_parts)
+    body_parts.append(line.strip())
 
 #first part of the syndrome phrase (i.e. disease-related adjectives)
 syndrome_1 = list()
 for line in open('syndrome.txt'):
-	syndrome_1.append(line.strip())
-random_syndrome_1_a = random.choice(syndrome_1)
-random_syndrome_1_b = random.choice(syndrome_1)
-random_syndrome_1_c = random.choice(syndrome_1)
-random_syndrome_1_d = random.choice(syndrome_1)
+    syndrome_1.append(line.strip())
 
 #second part of the syndrome phrase
 syndrome_2 = list()
 for line in open('syndrome_2.txt'):
-	syndrome_2.append(line.strip())
-random_syndrome_a = random.choice(syndrome_2)
-random_syndrome_b = random.choice(syndrome_2)
+    syndrome_2.append(line.strip())
 
 #adverbs
 adverbs = list()
 for line in open('adverbs.txt'):
-	adverbs.append(line.strip())
-random_adverb = random.choice(adverbs).capitalize()
+    adverbs.append(line.strip())
 
 #existing drugs
 existing_drugs = list()
 for line in open('existing_drugs.txt'):
-	existing_drugs.append(line.strip())
-random_existing_drug_a = random.choice(existing_drugs).capitalize()
-random_existing_drug_b = random.choice(existing_drugs).capitalize()
+    existing_drugs.append(line.strip())
 
 #scientific fields
 sciences = list()
 for line in open('sciences.txt'):
-	sciences.append(line.strip())
-random_science = random.choice(sciences)
+    sciences.append(line.strip())
 
 #side effects
 side_effects = list()
 for line in open('side_effects.txt'):
-	side_effects.append(line.strip())
-random_side_effect_a = random.choice(side_effects)
-random_side_effect_b = random.choice(side_effects)
-random_side_effect_c = random.choice(side_effects)
-random_side_effect_d = random.choice(side_effects)
+    side_effects.append(line.strip())
 
-number = str(random.randint(0,12))
+number = str(random.randint(0, 12))
 
 print
-print "Welcome to SmartPharmacist, " + name +"." 
+print "Welcome to SmartPharmacist, " + name +"."
 print
-print "Based on our analysis of your condition, we would suggest you start with a low dosage of "+drug_name+" to treat your "+complete_word+"."
+print "Based on our analysis of your condition, we would suggest you start with a low dosage of "+drug_name+" to treat your "+complete_disorder+"."
 print
-print drug_name+" is a "+random_drug_adj+" drug that is "+random_description_sentence+" "+complete_word+" and "+random_syndrome_1_a+" "+random_body_part+" "+random_syndrome_b+"." 
+print drug_name+" is a "+randomize(adjectives)+" drug that is "+randomize(description_sentence)+" "+complete_disorder+" and "+randomize(syndrome_1)+" "+randomize(body_parts)+" "+randomize(syndrome_2)+"."
 print
-print "Suggested daily dosage is "+number+" pills a day taken orally, or rubbing the gel form of the drug on your "+random_syndrome_1_d+" "+random_body_part_d+"."
+print "Suggested daily dosage is "+number+" pills a day taken orally, or rubbing the gel form of the drug on your "+randomize(syndrome_1)+" "+randomize(body_parts)+"."
 print
-print random_adverb+", individuals suffering from "+complete_word+" "+random_syndrome_a+" were prescribed "+random_existing_drug_a+" and "+random_existing_drug_b+", but new advancements in the field of "+random_body_part+" "+random_science+" has helped doctors better remedy this disorder."
+print randomize(adverbs)+", individuals suffering from "+complete_disorder+" "+randomize(syndrome_2)+" were prescribed "+randomize(existing_drugs)+" and "+randomize(existing_drugs)+", but new advancements in the field of "+randomize(body_parts)+" "+randomize(sciences)+" has helped doctors better remedy this disorder."
 print
-print "Side effects of "+drug_name+" may include: "+random_side_effect_a+", "+random_syndrome_1_b+" "+random_body_part_b+", "+random_side_effect_b+", "+random_side_effect_c+", "+random_syndrome_1_c+" "+random_body_part_c+", and "+random_side_effect_d+"."
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print "Side effects of "+drug_name+" may include: "+randomize(side_effects)+", "+randomize(syndrome_1)+" "+randomize(body_parts)+", "+randomize(side_effects)+", "+randomize(side_effects)+", "+randomize(syndrome_1)+" "+randomize(body_parts)+", and "+randomize(side_effects)+"."
